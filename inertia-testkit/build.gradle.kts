2e88f961c5dcd0555fa2c9b2075b079ad9fd28f4
Chamogelastos <stavros.ssta@gmail.com>
2026-06-18 13:03:24 +0300

    Added JUnit for testing

---
+++ inertia-testkit/build.gradle.kts
@@ -0,0 +1,14 @@
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.1.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events ;"passed"; "skipped"; "failed"
+        }
+    }
+}
